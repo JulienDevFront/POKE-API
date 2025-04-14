b418f51222be758363c1dc2ab7b0fd2cdc04defc
julien genty <mp.dev.mg@gmail.com>
2025-04-14 12:43:27 +0200

    Training on Express routes

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const pokemons = require("./pokemons.js");
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => res.send("Hello , Express ! 👋"));
+app.get('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const pokemon = pokemons.find(pokemon => pokemon.id === id);
+    res.send(`Vous avez demandé le pokémon ${pokemon.name}`);
+});
+
+app.get('/api/pokemons', (req, res) => {
+    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, pour le moment.`)
+})
 
 app.listen(port, () => console.log(`Notre application Node est démarrée sur : http://localhost:${port}`));
